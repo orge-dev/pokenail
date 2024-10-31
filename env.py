bddb1585559fb69c8527907dca930730f8933981
mikr ess <mikress@mikrs-Mac-Studio.local>
2024-10-31 14:06:01 -0500

    This commit updates envriomnet env

--- env.py
+++ env.py
@@ -3,6 +3,8 @@ from abc import ABC, abstractmethod
 from game_controller import GameController
 from config import ROM_PATH, EMULATION_SPEED  # Ensure these are defined in config
 
+from global_map import local_to_global, GLOBAL_MAP_SHAPE
+
 class AbstractEnvironment(ABC):
     @abstractmethod
     def reset(self):
@@ -13,7 +15,21 @@ class AbstractEnvironment(ABC):
     def step(self, action):
         """Take an action in the environment and return the next state, reward, done, and additional info."""
         pass
+    def get_game_coords(self):
+        return (self.read_m(0xD362), self.read_m(0xD361), self.read_m(0xD35E))
 
+
+    def get_global_coords(self):
+        x_pos, y_pos, map_n = self.get_game_coords()
+        return local_to_global(y_pos, x_pos, map_n)
+
+    def update_explore_map(self):
+        c = self.get_global_coords()
+        if c[0] >= self.explore_map.shape[0] or c[1] >= self.explore_map.shape[1]:
+            print(f"coord out of bounds! global: {c} game: {self.get_game_coords()}")
+            pass
+        else:
+            self.explore_map[c[0], c[1]] = 255
 class env_red(AbstractEnvironment):
     def __init__(self):
         self.controller = GameController(ROM_PATH, EMULATION_SPEED)
